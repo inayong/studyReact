2e98dff30e0b467e6175eb361c5e38a715189b46
inayong <141033984+inayong@users.noreply.github.com>
2023-09-20 12:21:58 +0900

    git init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// import Clock from './01/Clock';
+// import Lotto from './02/Lotto';
+// import MyCom from './03/MyCom';
+// import MyDiv from './03/MyDiv';
+import Box from './04/Box';
+
+function App() {
+  return (
+    // <Clock />
+    // <Lotto />
+    // <MyCom />
+    // <MyDiv />
+    <Box />
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App'; //내가만든 태그
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// const tick = () => {
+  root.render(
+    // <React.StrictMode>
+      <App />
+    // </React.StrictMode>
+  )
+// };
+// setInterval(() => tick(), 1000);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals(console.log);
